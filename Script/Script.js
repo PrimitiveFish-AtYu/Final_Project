23ffb9b06b29c97dd5888edc25cf38d2ee2dce6f
nptuYuJa <cbb110124@nptu.edu.tw>
2022-01-10 13:09:15 +0800

    More hover

--- Script/Script.js
+++ Script/Script.js
@@ -1,14 +1,21 @@
 // 控制accordiond開關
 function accordionStateSwitch(state) {
 
-
+    let accordionButtonlist = document.querySelectorAll('#service .accordion-button')
     var accordionlist = document.querySelectorAll('#service .accordion-collapse')
     var i = 0;
     for (i = 0; i < accordionlist.length; i++) {
         if (state)
+        {
             accordionlist[i].classList.add('show');
+            accordionButtonlist[i].classList.remove('collapsed');
+        }
         else
+        {
+            accordionButtonlist[i].classList.add('collapsed');
             accordionlist[i].classList.remove('show');
+
+        }
     }
 
 
